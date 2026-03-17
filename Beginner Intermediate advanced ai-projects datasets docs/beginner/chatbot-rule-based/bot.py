508a6bedb8dfa59bafec278816a61026fed49182
dev-abinash-sharma <77343564+dev-abinash-sharma@users.noreply.github.com>
2026-03-17 21:44:02 +0530

    Update bot.py

--- Beginner Intermediate advanced ai-projects datasets docs/beginner/chatbot-rule-based/bot.py
+++ Beginner Intermediate advanced ai-projects datasets docs/beginner/chatbot-rule-based/bot.py
@@ -1,1 +1,9 @@
-
+while True:
+    msg = input("You: ").lower()
+    if "hi" in msg:
+        print("Bot: Hello!")
+    elif "bye" in msg:
+        print("Bot: Goodbye!")
+        break
+    else:
+        print("Bot: I don't understand.")
